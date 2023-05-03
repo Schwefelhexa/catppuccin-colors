be6306a0a976c0a462afed3793d8e3ab8609e342
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-05-03 14:32:08 +0200

    Remove drop shadows

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -8,7 +8,7 @@ export default function Color({ color }: ColorProps) {
   return (
     <div className="w-32 h-32">
       <div
-        className="w-full h-full hover:scale-110 transition-transform ease-in-out bg-[--bg] rounded-lg shadow-xl shadow-[--shadow]"
+        className="w-full h-full hover:scale-110 transition-transform ease-in-out bg-[--bg] rounded-lg"
         style={{ "--bg": color.hex, "--shadow": `rgba(${color.raw}, 0.5)` }}
       />
     </div>
