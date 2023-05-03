5b2738cf39e7eba45e5466a19d8256f213e230cc
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-05-03 14:06:05 +0200

    Fancy color display

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -5,6 +5,6 @@ interface ColorProps {
   color: ColorDefinition
 }
 export default function Color({ color }: ColorProps) {
-  return <div className="bg-[--bg] w-32 h-32" style={{ "--bg": color.hex }} />
+  return <div className="bg-[--bg] w-32 h-32 rounded-lg shadow-xl shadow-[--shadow]" style={{ "--bg": color.hex, "--shadow": `rgba(${color.raw}, 0.5)` }} />
 }
 
