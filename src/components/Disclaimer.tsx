4c9c558bb226596b5b5c8bdec698c7b4b84ad813
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-05-03 15:07:03 +0200

    Add Disclaimer

---
+++ src/components/Disclaimer.tsx
@@ -0,0 +1,6 @@
+export default function Disclaimer() {
+  return <p className="text-text px-6 text-center">
+    This is a personal site, and not affiliated with Catppuccin. It is under the <a href="https://mit-license.org/">MIT License</a>. <br /> Check out <a href="https://github.com/Schwefelhexa/catppuccin-colors">the code</a>.
+  </p>
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Disclaimer from '@/components/Disclaimer'
 import Palette from '@/components/Palette'
 import paletteRaw from '@/palette.json'
 import { AllPalettes, paletteNames } from '@/types/palette'
@@ -6,12 +7,12 @@ const palette = paletteRaw as AllPalettes
 
 export default function Home() {
   return (
-    <main className="h-full overflow-scroll px-4 py-6">
-
+    <main className="h-full overflow-scroll px-8 py-6">
       <div className="flex flex-col items-center gap-16">
         {paletteNames.map(name => (
           <Palette key={name} name={name} palette={palette[name]} />
         ))}
+        <Disclaimer />
       </div>
     </main>
   )
