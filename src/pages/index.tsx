ce6aaf843e6f6e674e9d64540f5439170fab8ff5
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-05-03 13:14:26 +0200

    Add palette definition from catppuccin repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+import palette from '@/palette.json'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
     <h1>Hello World</h1>
+    <p>{JSON.stringify(palette)}</p>
+    </>
   )
 }
