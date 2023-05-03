12fdb485d68d53e0a56e10d65d25577f8e1cff63
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-05-03 15:10:33 +0200

    Fix prop type errors

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,6 +1,7 @@
 import { ColorDefinition } from "@/types/palette"
 import { usePress } from 'react-aria'
-import { motion } from "framer-motion"
+import { HTMLMotionProps, motion } from "framer-motion"
+import type { CSSProperties } from "react"
 
 interface ColorProps {
   name: string
@@ -14,10 +15,10 @@ export default function Color({ color }: ColorProps) {
   return (
     <div className="w-24 h-24 md:w-32 md:h-32">
       <motion.div
-        {...pressProps}
+        {...pressProps as HTMLMotionProps<'div'>}
         role="button"
         className="w-full h-full bg-[--bg] rounded-lg focus:outline-none"
-        style={{ "--bg": color.hex, "--shadow": `rgba(${color.raw}, 0.5)` }}
+        style={{ "--bg": color.hex } as CSSProperties}
         whileHover={{ scale: 1.1, rotate: -5 }}
         whileTap={{ scale: 0.9, rotate: 5 }}
       />
